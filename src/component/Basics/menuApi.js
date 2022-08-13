81db8b2f8940743ca6ee5fcda38280ab836b88a0
ayu3warke <ayuwarke3@gmail.com>
2022-08-13 12:54:47 +0530

    first commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,184 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/bread upma.jpg",
+      name: "Bread Upma",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/masala dosa.jpg",
+      name: "Masala Dosa",
+      category: "breakfast",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/idli sambar.jpg",
+      name: "Idli Sambar",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/mix veg paratha.jpg",
+      name: "Mix Veg Paratha",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/egg toast.jpg",
+      name: "Egg Toast",
+      category: "breakfast",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/egg cury.jpg",
+      name: "Shahi Egg Curry",
+      category: "lunch",
+      price: "180₹",
+      description:
+        "Eggs are a versatile food that can be cooked for any meal of the day. From breakfast to dinner, it can be a go-to food. Here is a mildly-spiced egg curry made with garlic, onions, a whole lot of kasuri methi, fresh cream, yogurt and fresh coriander. It can be paired with plain chapati, paratha, rice, biryani or pulao depending upon your choice. You can also serve pickle along with it ",
+    },
+    {
+      id: 7,
+      image: "../images/Dahi chicken.jpg",
+      name: "Dahi Chicken",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "A low-fat chicken curry made with marinated pieces of chicken, turmeric, red chilli, garlic paste, green chilli, tomatoes and fresh coriander. A perfect chicken recipe to cook for lunch, pair with rice or chapati. Relish your favourite chicken, guilt-free!  ",
+    },
+    {
+      id: 8,
+      image: "../images/chana kulcha.webp",
+      name: "Chana Kulcha",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "A classic dish that never goes out of style. The quintessential chana kulcha  needs only a few ingredients - cumin powder, ginger, coriander powder, carom powder and some mango powder, which is what gives the chana it's sour and tangy taste.",
+    },
+    {
+      id: 9,
+      image: "../images/masala bhindi.webp",
+      name: "Masala Bhindi",
+      category: "lunch",
+      price: "10₹",
+      description:"A delicious dish made with freshly bought okra. Grab some mustard oil, cumin seeds, fennel seeds, finely-chopped onions, ginger and a pinch of amchoor.",
+    },
+    {
+      id: 10,
+      image: "../images/urlai roast.jpg",
+      name: "Urlai Roast",
+      category: "lunch",
+      price: "10₹",
+      description:
+        "Oh, this one's a real delight! Perfectly boiled baby potatoes cooked in a peppery Chettinad masala, onions and tomatoes. ",
+    },
+    {
+      id: 11,
+      image: "../images/fritters.jpg",
+      name: "Fritters",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      image: "../images/paneer bread roll.jpg",
+      name: "Paneer Bread Roll",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 13,
+      image: "../images/vegetable macroni.jpg",
+      name: "Vegetable Macroni",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 14,
+      image: "../images/cheese garlic bread.jpg",
+      name: "Cheese Garlic Bread",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 15,
+      image: "../images/uttapam.jpg",
+      name: "Uttapam",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 16,
+      image: "../images/dum aloo.jpg",
+      name: "Dum Aloo",
+      category: "dinner",
+      price: "50₹",
+      description:
+        "Your family will thank you for this absolutely fantastic bowl of dum aloo cooked Lakhnawi style. ",
+    },
+    {
+      id: 17,
+      image: "../images/dum panner.jpg",
+      name: "Dum Paneer",
+      category: "dinner",
+      price: "50₹",
+      description:
+        "A lovely take on the unusual paneer gravy: Small chunks of paneer cooked with cinnamon, fried onions, black pepper, ginger, garlic and yogurt to make this gorgeous and fragrant curry. Paneer cubes are cooked in flavourful spices in a dum or pressure, where steam can't release, making it a dish bursting with aromatic flavours.  ",
+    },
+    {
+      id: 18,
+      image: "../images/makhmali kofte.jpg",
+      name: "Makhmali Kofte",
+      category: "dinner",
+      price: "50₹",
+      description:
+      "A rich gravy made of khus khus, coconut and milk that tastes best with koftas made from khoya. This velvety and creamy recipe will leave you licking your fingers. ",
+    },
+    {
+      id: 19,
+      image: "../images/lemon chicken.jpg",
+      name: "Lemon Chicken",
+      category: "dinner",
+      price: "50₹",
+      description:
+        " Try this different yet amazingly delicious version of a chicken at your next dinner party and you wouldn't be able to resist coming back to this chicken recipe! ",
+    },
+    {
+      id: 20,
+      image: "../images/keema biryani.webp",
+      name: "Keema Biryani",
+      category: "dinner",
+      price: "50₹",
+      description:
+        "A luscious biryani made with minced meat, almonds, raisins, yogurt, onions, garlic, ginger and milk. It's one of those dishes which don't just make your dinner table look good, but also leave your house smelling fabulous.. ",
+    },
+  ];
+  
+  export default Menu;
